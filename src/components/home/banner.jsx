2ce60c102631900b60aabb4b557f4e54dc2623c0
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2021-12-12 20:44:56 +0100

    new

--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { HomeBannerImage } from '../svg'
+import { useRouter } from 'next/router'
 
 export const Banner = (props) => {
+    const router = useRouter()
+
     return (
         <div className=" flex container py-10 px-7 mx-auto flex-col-reverse justify-center items-center lg:flex-row">
             <div className="flex-1 py-5">
@@ -13,11 +16,17 @@ export const Banner = (props) => {
                     offrons.
                 </p>
                 <div className="flex flex-col sm:flex-row justify-between sm:space-x-4 space-y-4 sm:space-y-0 w-full">
-                    <button className="flex-1 transition hover:shadow-md duration-300 bg-purple-700 hover:bg-purple-900 py-1 px-4 text-white font-medium rounded-lg text-xl">
+                    <button
+                        onClick={() => router.push('/login?subscribe=true')}
+                        className="flex-1 transition hover:shadow-md duration-300 bg-purple-700 hover:bg-purple-900 py-1 px-4 text-white font-medium rounded-lg text-xl"
+                    >
                         Créer un compte
                     </button>
-                    <button className="flex-1 transition hover:shadow-md duration-300 border-2 border-purple-700 hover:bg-purple-100 py-1 px-4 text-purple-700 font-medium rounded-lg text-xl">
-                        Voir les tutoriels
+                    <button
+                        onClick={() => router.push('/formations')}
+                        className="flex-1 transition hover:shadow-md duration-300 border-2 border-purple-700 hover:bg-purple-100 py-1 px-4 text-purple-700 font-medium rounded-lg text-xl"
+                    >
+                        Voir les formations
                     </button>
                 </div>
             </div>
--- src/components/home/section2.jsx
+++ src/components/home/section2.jsx
@@ -1,7 +1,11 @@
+import router from 'next/router'
 import React from 'react'
 import { HomeSaly } from '../svg'
+import { useRouter } from 'next/router'
 
 export const Section2 = (props) => {
+    const router = useRouter()
+
     return (
         <div className=" flex container py-10 px-7 mx-auto flex-col justify-center items-center lg:flex-row">
             <div className="flex-1">
@@ -17,10 +21,16 @@ export const Section2 = (props) => {
                     rithme et en fonction de votre disponibilité.
                 </p>
                 <div className="flex flex-col sm:flex-row justify-between sm:space-x-4 space-y-4 sm:space-y-0 w-full">
-                    <button className="flex-1 transition hover:shadow-md duration-300 bg-purple-700 hover:bg-purple-900 py-1 px-4 text-white font-medium rounded-lg text-xl">
+                    <button
+                        onClick={() => router.push('/login?subscribe=true')}
+                        className="flex-1 transition hover:shadow-md duration-300 bg-purple-700 hover:bg-purple-900 py-1 px-4 text-white font-medium rounded-lg text-xl"
+                    >
                         Créer un compte
                     </button>
-                    <button className="flex-1 transition hover:shadow-md duration-300 border-2 border-purple-700 hover:bg-purple-100 py-1 px-4 text-purple-700 font-medium rounded-lg text-xl">
+                    <button
+                        onClick={() => router.push('/formations')}
+                        className="flex-1 transition hover:shadow-md duration-300 border-2 border-purple-700 hover:bg-purple-100 py-1 px-4 text-purple-700 font-medium rounded-lg text-xl"
+                    >
                         Voir les formations
                     </button>
                 </div>
